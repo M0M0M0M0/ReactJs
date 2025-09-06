eb13e599b57670c7fa3acbce33c907da835f3566
momo <dangvanquocmomo@gmail.com>
2025-09-06 21:04:43 +0700

    Fix routing for GitHub Pages - add basename and 404.html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import ProductDetail from "./pages/ProductDetail";
@@ -10,20 +10,22 @@ import { CartProvider } from "./context/CartContext";
 function App() {
   return (
     <CartProvider>
-      <div className="App">
-        <NavbarComponent />
-        <main>
-          <div className="container">
-            <Routes>
-              <Route path="/" Component={Home} />
-              <Route path="/category/:slug" element={<Category />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/Cart" element={<Cart />} />
-              <Route path="/forecast" element={<Forecast />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
+      <BrowserRouter basename="/ReactJs">
+        <div className="App">
+          <NavbarComponent />
+          <main>
+            <div className="container">
+              <Routes>
+                <Route path="/" Component={Home} />
+                <Route path="/category/:slug" element={<Category />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/Cart" element={<Cart />} />
+                <Route path="/forecast" element={<Forecast />} />
+              </Routes>
+            </div>
+          </main>
+        </div>
+      </BrowserRouter>
     </CartProvider>
   );
 }
